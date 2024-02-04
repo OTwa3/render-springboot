9be0efc17e810e55e72b027c31fc9d9ef9cea2cf
Owen Twa <owentwa3@gmail.com>
2024-02-03 20:39:10 -0800

    added Dockerfile

---
+++ test/src/test/java/_om/example/test/TestApplicationTests.java
@@ -0,0 +1,13 @@
+package _om.example.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
